1b2863ef62ede9bfe91f2654be5c6a32c15518f3
Katherine Mantel <kmantel@princeton.edu>
2018-10-08 16:36:50 -0400

    Parameters: add more to various classes

--- psyneulink/core/components/component.py
+++ psyneulink/core/components/component.py
@@ -1411,8 +1411,6 @@ class Component(object, metaclass=ComponentsMeta):
         value = Param(np.array([0]), read_only=True)
         context = Param(None, user=False)
 
-        function = Param(None, stateful=False)
-
         def _parse_variable(self, variable):
             return variable
 
@@ -1585,7 +1583,11 @@ class Component(object, metaclass=ComponentsMeta):
             if FUNCTION in param_defaults and param_defaults[FUNCTION] is not None:
                 function = param_defaults[FUNCTION]
             else:
-                function = self.ClassDefaults.function
+                try:
+                    function = self.ClassDefaults.function
+                except AttributeError:
+                    # assume function is a method on self
+                    pass
         try:
             function_params = param_defaults[FUNCTION_PARAMS]
         except KeyError:
